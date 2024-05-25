e8ffeb5e0f23aa9e22c6786023784c7985020197
shawnnygoh <shawnnygoh@gmail.com>
2024-05-25 13:59:24 +0800

    feat: add .gitattributes file to ignore html and makefile

---
+++ guessing_game/target/debug/deps/guessing_game-3c531c7dc6b2d8a2.d
@@ -0,0 +1,5 @@
+/Users/shawnnygoh/github-repos/rusty-dev/guessing_game/target/debug/deps/libguessing_game-3c531c7dc6b2d8a2.rmeta: src/main.rs
+
+/Users/shawnnygoh/github-repos/rusty-dev/guessing_game/target/debug/deps/guessing_game-3c531c7dc6b2d8a2.d: src/main.rs
+
+src/main.rs:
---
+++ guessing_game/target/debug/deps/guessing_game-9c36a9db48be7588.d
@@ -0,0 +1,5 @@
+/Users/shawnnygoh/github-repos/rusty-dev/guessing_game/target/debug/deps/libguessing_game-9c36a9db48be7588.rmeta: src/main.rs
+
+/Users/shawnnygoh/github-repos/rusty-dev/guessing_game/target/debug/deps/guessing_game-9c36a9db48be7588.d: src/main.rs
+
+src/main.rs:
